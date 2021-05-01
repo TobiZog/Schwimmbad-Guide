95f1880b7a5ed11c868df43559128e0f554cbd42
tobias <developer@zoghaib.de>
2021-05-01 22:41:31 +0200

    Adding GPS support

--- app/src/main/java/de/zoghaib/schwimmbadguide/MapFragment.kt
+++ app/src/main/java/de/zoghaib/schwimmbadguide/MapFragment.kt
@@ -1,7 +1,11 @@
 package de.zoghaib.schwimmbadguide
 
+import android.Manifest
+import android.content.pm.PackageManager.PERMISSION_GRANTED
+import android.location.LocationManager
 import android.os.Bundle
 import android.view.View
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -17,50 +21,84 @@ import de.zoghaib.schwimmbadguide.databinding.FragmentMapBinding
  */
 class MapFragment : Fragment(R.layout.fragment_map), OnMapReadyCallback {
 
-    /* -------------------- Member Variables -------------------- */
+	/* -------------------- Member Variables -------------------- */
 
-    /** View binding object to access items in the view */
-    private lateinit var binding : FragmentMapBinding
+	/** View binding object to access items in the view */
+	private lateinit var binding : FragmentMapBinding
 
-    /** The map object in the view */
-    private lateinit var mMap : GoogleMap
-
-
-    /* -------------------- Lifecycle -------------------- */
-
-    /**
-     * Lifecycle method after the view was created
-     *
-     * @param   savedInstanceState      Save state of the view
-     */
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        // Set up binding
-        binding = FragmentMapBinding.bind(view)
+	/** The map object in the view */
+	private lateinit var mMap : GoogleMap
 
 
-        // Set up map view
-        binding.mvMap.onCreate(savedInstanceState)
-        binding.mvMap.onResume()
-        binding.mvMap.getMapAsync(this)
-    }
+	/* -------------------- Lifecycle -------------------- */
+
+	/**
+	 * Lifecycle method after the view was created
+	 *
+	 * @param   savedInstanceState      Save state of the view
+	 */
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+
+		// Set up binding
+		binding = FragmentMapBinding.bind(view)
 
 
-    /**
-     * Lifecycle method after the map is ready in the view
-     *
-     * @param   googleMap       Google Map object to interact with
-     */
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
+		// Set up map view
+		binding.mvMap.onCreate(savedInstanceState)
+		binding.mvMap.onResume()
+		binding.mvMap.getMapAsync(this)
+	}
 
-        // Set up map view
-        // todo: GPS service
-        mMap.uiSettings.isZoomControlsEnabled = true
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(52.374481, 9.738414), 11.0f))
-        //mMap.isMyLocationEnabled = true
 
-        // setPins()
-    }
+	/**
+	 * Lifecycle method after the map is ready in the view
+	 *
+	 * @param   googleMap       Google Map object to interact with
+	 */
+	override fun onMapReady(googleMap: GoogleMap) {
+		mMap = googleMap
+
+		// Request permissions
+		if(ActivityCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PERMISSION_GRANTED) {
+			requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 42)
+		} else {
+			moveCameraToPosition()
+		}
+
+		// Set up map view
+		mMap.uiSettings.isZoomControlsEnabled = true
+		mMap.uiSettings.isMapToolbarEnabled = true
+
+	}
+
+
+	/**
+	 * Get the position and move the map camera to this
+	 */
+	@Throws(SecurityException::class)
+	private fun moveCameraToPosition() {
+		val m = context!!.getSystemService(LocationManager::class.java)
+
+		val loc = m.getLastKnownLocation(LocationManager.PASSIVE_PROVIDER)
+		loc?.run {
+			mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(latitude, longitude), 15f))
+		}
+
+		mMap.isMyLocationEnabled = true
+	}
+
+
+	/**
+	 * Lifecycle method if the user allow or disallow permissions
+	 *
+	 * @param   requestCode     Code which are send on requestPermissions
+	 * @param   permissions     Array of requested permissions
+	 * @param   grantResults    Array of results
+	 */
+	override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>, grantResults: IntArray) {
+		if (requestCode == 42 && grantResults.isNotEmpty() && grantResults[0] == PERMISSION_GRANTED) {
+			moveCameraToPosition()
+		}
+	}
 }
