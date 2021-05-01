ba538cc315a2d0c02170acf6871d9b2bf548e89e
tobias <developer@zoghaib.de>
2021-05-01 19:15:48 +0200

    Adding map view

---
+++ README.md
@@ -0,0 +1,14 @@
+# Schwimmbad Guide
+
+## Installation
+1. Download the latest files
+2. Add Google Maps API Key to your gradle.properties file:
+    - Get your personal API Key from https://console.cloud.google.com
+    - Go to your Personal Folder/.gradle/ and open gradle.properties (Add file, if there is no one)
+    - Adding code like these:
+    ```
+    <gradle.properties>
+    Schwimmbad_Guide_API_KEY = "thisIsTheApiKey"
+    </gradle.properties>
+    ```
+3. Start Android Studio and have fun with the code!
--- app/src/main/java/de/zoghaib/schwimmbadguide/MapFragment.kt
+++ app/src/main/java/de/zoghaib/schwimmbadguide/MapFragment.kt
@@ -1,6 +1,61 @@
 package de.zoghaib.schwimmbadguide
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import de.zoghaib.schwimmbadguide.databinding.FragmentMapBinding
 
-class MapFragment : Fragment(R.layout.fragment_map) {
+/**
+ * Fragment which shows a Google Maps view with location of the pools
+ *
+ * @author  Tobias Zoghaib
+ * @since   2021-05-01
+ */
+class MapFragment : Fragment(R.layout.fragment_map), OnMapReadyCallback {
+
+    /* -------------------- Member Variables -------------------- */
+
+    /** View binding object to access items in the view */
+    private lateinit var binding : FragmentMapBinding
+
+    /** The map object in the view */
+    private lateinit var mMap : GoogleMap
+
+
+    /* -------------------- Lifecycle -------------------- */
+
+    /**
+     * Lifecycle method after the view was created
+     *
+     * @param   savedInstanceState      Save state of the view
+     */
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Set up binding
+        binding = FragmentMapBinding.bind(view)
+
+
+        // Set up map view
+        binding.mvMap.onCreate(savedInstanceState)
+        binding.mvMap.onResume()
+        binding.mvMap.getMapAsync(this)
+    }
+
+
+    /**
+     * Lifecycle method after the map is ready in the view
+     *
+     * @param   googleMap       Google Map object to interact with
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Set up map view -> todo!
+        // setPins()
+        // setViews()
+    }
 }
